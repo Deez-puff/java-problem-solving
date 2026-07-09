45663f819c638a6d4fd387870868395eeda620a3
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-10 00:10:24 +0530

    Add Comments class demonstrating comment usage

---
+++ 01-Basics/03_comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("Single-line comment");
+
+        /*
+           This is a
+           multi-line comment.
+        */
+
+        System.out.println("Multi-line comment");
+
+    }
+
+}
