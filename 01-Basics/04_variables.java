39ed2d5b32de613497ff08c73aa71f94a519bbcd
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-10 00:10:56 +0530

    Create 04_variables.java

---
+++ 01-Basics/04_variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        String name = "Deepak";
+        int age = 20;
+        double cgpa = 8.91;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("CGPA : " + cgpa);
+        System.out.println("Grade : " + grade);
+        System.out.println("Passed : " + passed);
+
+    }
+
+}
