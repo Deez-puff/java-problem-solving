aa05f5821447675df2c73015bdc94d9ab693b994
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-10 00:11:22 +0530

    Create 05_DataTypes.java

---
+++ 01-Basics/05_DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short s = 30000;
+        int i = 500000;
+        long l = 999999999L;
+
+        float f = 10.5f;
+        double d = 25.789;
+
+        char c = 'J';
+
+        boolean flag = true;
+
+        System.out.println("byte = " + b);
+        System.out.println("short = " + s);
+        System.out.println("int = " + i);
+        System.out.println("long = " + l);
+        System.out.println("float = " + f);
+        System.out.println("double = " + d);
+        System.out.println("char = " + c);
+        System.out.println("boolean = " + flag);
+
+    }
+
+}
