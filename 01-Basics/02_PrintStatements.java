3b7dd3de957cc6e63483da0621c4ec9b738f86e2
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-10 00:08:51 +0530

    Update 02_PrintStatements.java

--- 01-Basics/02_PrintStatements.java
+++ 01-Basics/02_PrintStatements.java
@@ -1,1 +1,19 @@
+public class PrintStatements {
 
+    public static void main(String[] args) {
+
+        System.out.println("Java Programming");
+        System.out.print("Welcome ");
+        System.out.print("to ");
+        System.out.print("GitHub");
+        System.out.println();
+
+        System.out.println("Learning Java is Fun!");
+
+    }
+
+}
+
+// Java Programming
+// Welcome to GitHub
+//Learning Java is Fun!
