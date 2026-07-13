81bfd60af4b9bc24bdc013b7b7bde28c08393df4
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-13 23:29:20 +0530

    Create TernaryOperator.java

---
+++ 02-Operators/TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        int marks = 72;
+
+        String result = (marks >= 50) ? "Pass" : "Fail";
+
+        System.out.println("Result: " + result);
+    }
+}
