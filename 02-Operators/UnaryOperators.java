6da5fa78f5a99a072c9c5e981601d502f5229203
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-13 23:28:44 +0530

    Create UnaryOperators.java

---
+++ 02-Operators/UnaryOperators.java
@@ -0,0 +1,20 @@
+public class UnaryOperators {
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        System.out.println("Original: " + x);
+
+        System.out.println("Pre Increment: " + (++x));
+
+        System.out.println("Post Increment: " + (x++));
+
+        System.out.println("Current Value: " + x);
+
+        System.out.println("Pre Decrement: " + (--x));
+
+        System.out.println("Post Decrement: " + (x--));
+
+        System.out.println("Final Value: " + x);
+    }
+}
