b43420e537a6c23e4dfa07ac187aa67943eb5d36
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-20 22:16:49 +0530

    Fix formatting in 04_variables.java

--- 01-Basics/04_variables.java
+++ 01-Basics/04_variables.java
@@ -7,7 +7,7 @@ public class Variables {
         double cgpa = 8.91;
         char grade = 'A';
         boolean passed = true;
-
+ 
         System.out.println("Name : " + name);
         System.out.println("Age : " + age);
         System.out.println("CGPA : " + cgpa);
