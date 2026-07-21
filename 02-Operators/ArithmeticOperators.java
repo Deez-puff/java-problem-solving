95988613a2979802409f595848b999200010fe41
Deepak Rajesh <deepakrajesh500@gmail.com>
2026-07-21 14:30:56 +0530

    Fix duplicate line for division output

--- 02-Operators/ArithmeticOperators.java
+++ 02-Operators/ArithmeticOperators.java
@@ -7,7 +7,7 @@ public class ArithmeticOperators {
         System.out.println("Addition: " + (a + b)); 
         System.out.println("Subtraction: " + (a - b));
         System.out.println("Multiplication: " + (a * b));
-        System.out.println("Division: " + (a / b));
+        System.out.println("Division: " + (a / b)); 
         System.out.println("Modulus: " + (a % b));
     }
 }
